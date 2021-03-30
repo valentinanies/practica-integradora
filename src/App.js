7adf5cd31b640abaafc423b1f82b228412fa5716
Valentina Nies <valunies@gmail.com>
2021-03-30 17:41:19 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="App">
+     <h1>Prueba de React</h1>
+     <ul>
+       <li>Item</li>
+       <li>Item</li>
+       <li>Item</li>
+       <li>Item</li>
+     </ul>
+
+     <p>Acá se genera el contenido de la página</p>
+    </div>
+  );
+}
+
+export default App;
